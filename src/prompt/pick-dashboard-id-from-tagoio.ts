691d762076512f49c359b487c5eac230b6bf7d22
Vitor Lima <vitor@ferreiradelima.com>
2023-09-01 11:23:41 -0300

    added dashboard copy-tab command

---
+++ src/prompt/pick-dashboard-id-from-tagoio.ts
@@ -0,0 +1,18 @@
+import prompts from "prompts";
+
+import { Account } from "@tago-io/sdk";
+
+async function pickDashboardIDFromTagoIO(account: Account, message: string = "Which dashboard you want to pick?") {
+  const deviceList = await account.dashboards.list({ amount: 100, fields: ["id", "label"] });
+
+  const { id } = await prompts({
+    message,
+    name: "id",
+    type: "autocomplete",
+    choices: deviceList.map((x) => ({ title: `${x.label} [${x.id}]`, value: x.id })),
+  });
+
+  return id as string;
+}
+
+export { pickDashboardIDFromTagoIO };
